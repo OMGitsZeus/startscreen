4297d9e00ea7ec75da23cdcd71ec8cca5b0bbded
OMGitsZeus <146347026+OMGitsZeus@users.noreply.github.com>
2026-01-13 21:36:15 +0000

    Play cinematic music via NUI

--- startscreen/client/web/js/hud.js
+++ startscreen/client/web/js/hud.js
@@ -1,55 +1,36 @@
-function betterNumber(x) {
-  return x.toLocaleString("fr-FR");
-}
-
-var lastPoints = 0;
-var lastMulti = "x0.0"
-
-
-
 $(function () {
+  const resourceName = GetParentResourceName ? GetParentResourceName() : "startscreen";
+
+  $("#containerJoin").show();
+
   window.addEventListener("message", function (event) {
-    var item = event.data;
-
-
-    if (item.ShowHud) {
-      // Drift points
-      $(".driftDisplay").html(betterNumber(item.driftPoints));
-      $(".driftDisplayMulti").html(item.driftDisplayMulti)
-
-
-      lastPoints = item.driftPoints
-      lastMulti = item.driftDisplayMulti
-
-      $("#container").fadeIn(500);
-    } else if (item.HideHud) {
-      // Hide GUI
-      $(".driftImage").html('<img class="animate__animated animate__hinge driftImage" src="images/drifting.png" />');
-      $(".driftDisplay").html('<div class="animate__animated animate__hinge driftDisplay"><span> ' + lastPoints +'</span></div>');
-      $(".driftDisplayMulti").html('<div class="animate__animated animate__hinge driftDisplayMulti"><span> ' + lastMulti + '</span></div> ');
-      $("#container").fadeOut(1000);
-    }
-
-    if (item.ShowSucces) { 
-      $("#containerSucces").fadeIn(100);
-      $(".succesText").html(item.label);
-    } else if (item.HideSucces) {
-      $("#containerSucces").fadeOut(500);
-    }
+    const item = event.data;
+    const music = document.getElementById("cinematicMusic");
 
     if (item.containerJoins) {
       $("#containerJoin").fadeIn(0);
-    } 
+    }
 
     if (item.joinClick) {
       $("#containerJoin").fadeOut(500);
     }
 
+    if (item.playMusic && music) {
+      music.pause();
+      music.currentTime = 0;
+      music.src = `audio/${item.playMusic}.ogg`;
+      music.volume = item.volume ?? 0.07;
+      music.play().catch(() => {});
+    }
 
+    if (item.stopMusic && music) {
+      music.pause();
+      music.currentTime = 0;
+    }
   });
 
-  $('.clickJoinButton').on('click', function (e) {
-		e.preventDefault();
-		$.post('https://driftV/joinServer', JSON.stringify(test = false));
-	});
+  $(".clickJoinButton").on("click", function (e) {
+    e.preventDefault();
+    $.post(`https://${resourceName}/joinServer`, JSON.stringify({}));
+  });
 });
